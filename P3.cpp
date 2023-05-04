3a001685375a8eac69c24a3c42c15a0d4a4d83a2
Ojas Arora <127867874+Ojas-Arora@users.noreply.github.com>
2023-05-04 22:57:57 +0530

    Add files via upload

---
+++ P3.cpp
@@ -0,0 +1,29 @@
+/* Write a program to design a class having name student with data members name,age,roll no 
+by using member functions to get and display data */
+#include<iostream>
+using namespace std;
+class student{
+    char name[30];
+    int rollno;
+    int age;
+    public:
+    void getdata(){
+        cout<<"Enter name of student"<<endl;
+        cin>>name;
+        cout<<"Enter roll no of student"<<endl;
+        cin>>rollno;
+        cout<<"Enter age of student"<<endl;
+        cin>>age;
+    } 
+    void displaydata(){
+        cout<<"Enter name of student"<<name<<endl;
+        cout<<"Enter roll no of student"<<rollno<<endl;
+        cout<<"Enter age of student"<<age<<endl;
+    }
+};
+int main(){
+    student s;
+    s.getdata();
+    s.displaydata();
+    return 0;
+}
